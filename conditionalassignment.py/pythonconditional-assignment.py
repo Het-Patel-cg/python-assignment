9ef5b5ee742117a29078949b70f4a37a03cb4222
Het Patel <hetpatel@example.com>
2026-09-07 09:31:14 +0530

    Add conditional assignment exercises

---
+++ conditionalassignment.py/pythonconditional-assignment.py
@@ -0,0 +1,131 @@
+number= 17
+
+if number >= 10:
+	print("Greater than 10")
+
+age= 19
+
+if age >= 18:
+	print("Adult")
+
+number= int(input("Enter a number:"))
+
+if number > 0:
+    print ("Positive")
+
+marks = 42
+
+if marks >= 40:
+    print ("Pass")
+
+number= int(input("Enter a number:"))
+
+if number == 0:
+      print ("Zero")
+
+number = 4
+
+if number >= 0:
+    print("You are Positive.")
+else:
+    print("Not Positive.")
+
+age = 16
+
+if age >= 18:
+    print("Adult.")
+else:
+    print("Minor.")
+
+a=6
+
+if int(a) % 2 == 0:
+    print("Even number")
+
+else:
+     print ("Odd number")
+
+marks = 50
+
+if marks >= 40:
+    print ("Pass")
+
+else:
+     print("Fail")
+
+
+number1 = 4
+number2 = 7
+
+if number1 > number2:
+    print ("number 1 is greater")
+
+else:
+     print ("number2 is greater")
+
+
+marks = 80
+
+if marks >= 90:
+     print ("A")
+elif marks >= 70:
+     print ("B")
+elif marks >= 50:
+     print("C")
+elif marks >= 40:
+     print ("D")
+else:
+     print ("F")
+
+
+
+number = 8
+
+if number > 0:
+     print ("Positive" )
+elif number < 0:
+     print ("Negative")
+else:
+     print ("Zero")
+
+day_number = input("Enter a number (1-5): ")
+
+if day_number == 1:
+     print ("Monday")
+elif day_number == 2:
+     print ("Tuesday")
+elif day_number == 3:
+     print ("Wednesday")
+elif day_number == 4:
+     print ("Thursday")
+elif day_number == 5:
+     print ("Friday")
+elif day_number == 6:
+     print ("Saturday")
+else:
+     print ("Other")
+
+marks = input ("Enter your marks:")
+
+if marks >= 90:
+     print ("excellent")
+elif marks >= 70:
+     print ("good")
+elif marks >= 33:
+     print ("pass")
+else:
+     print ("fail")
+
+
+number = int(input("Enter a number:"))
+
+if number == 1:
+     print ("1")
+elif number == 2:
+     print ("2")
+elif number == 3:
+     print ("3")
+else:
+     print ("other")
+
+	
